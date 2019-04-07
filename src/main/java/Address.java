b6768ff29107b429113603c45f82fa74aa2f981e
suharki <bar.kedzierska@gmail.com>
2019-04-07 13:40:02 +0200

    First commit: 	- add classes storing data: Person, Address 	- add Main class 	- add empty Dao class

---
+++ src/main/java/Address.java
@@ -0,0 +1,8 @@
+public class Address {
+
+    private String city;
+    private String zipCode;
+    private String street;
+    private int homeNumber;
+
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,10 @@
+public class Person {
+
+    private String name;
+    private String surname;
+    private int age;
+    private int height;
+    private Double weight;
+    private Address address;
+
+}
